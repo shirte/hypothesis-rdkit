29e549b6e0a67dc4ae539ed79f359afa27c77c75
Steffen Hirte <shirte@users.noreply.github.com>
2023-05-22 13:01:29 +0200

    Do not force installing RDKit

--- setup.py
+++ setup.py
@@ -3,19 +3,26 @@ from setuptools import setup, find_packages
 
 setup(
     name="hypothesis-rdkit",
-    version="0.1",
+    version="0.2",
     maintainer="Steffen Hirte",
     maintainer_email="shirte@users.noreply.github.com",
     packages=find_packages(),
     url="https://github.com/shirte/hypothesis-rdkit",
     long_description=open("README.md").read(),
-    install_requires=["hypothesis", "rdkit"],
+    long_description_content_type="text/markdown",
+    license="MIT",
+    license_files=("LICENSE",),
+    install_requires=["hypothesis"],
+    extras_require={"dev": ["black", "isort"]},
     entry_points={"hypothesis": {"_ = hypothesis_rdkit.hook:_hypothesis_setup_hook"}},
     include_package_data=True,
     classifiers=[
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Testing",
+        "Topic :: Scientific/Engineering :: Chemistry",
         "Framework :: Hypothesis",
+        "License :: OSI Approved :: MIT License",
     ],
 )
